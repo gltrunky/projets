0933e083ffae9191b331d74af2546505137a2efe
Alex <almartin86@yahoo.fr>
2020-02-26 22:34:08 +0100

    connexion js+css

--- gameLegend/index.php
+++ gameLegend/index.php
@@ -8,13 +8,16 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Questrial&display=swap">
-    <link rel="stylesheet" href="/component/css/mesCssPerso.css">
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <!-- Mon css -->
+    <link rel="stylesheet" href="./component/css/style.css">
     <!-- Titre de l'onglet -->
     <title>Game Legend</title>
 </head>
 <body>
+
+    <!-- Mon script -->
+    <script src="./component/js/app.js"></script>
     <!-- Script -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" crossorigin="anonymous"></script>
