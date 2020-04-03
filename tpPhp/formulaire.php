4cf036b12aa0b68456c3f6becebc432805a3705c
Alex <almartin86@yahoo.fr>
2020-04-03 11:15:56 +0200

    Exercices Openclassroom

---
+++ tpPhp/formulaire.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>la page protégée</title>
+</head>
+
+<body>
+    <h1>La page protégée</h1>
+    <form action="secret.php" method="POST">
+        <input type="text" placeholder="pseudo" name="pseudo">
+        <input type="password" placeholder="Mdp" name="password">
+        <input type="submit">
+    </form>
+</body>
+
+</html>
---
+++ tpPhp/secret.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>La page secrete</title>
+</head>
+
+<body>
+    <?php
+    if (isset($_POST['password']) and $_POST['password'] ==  "kangourou") {
+    ?>
+        <h1>La page secrete</h1>
+        <p>Bienvenue sur la page secrete <?php echo htmlspecialchars($_POST['pseudo']) ?></p>
+        <?php ?>
+</body>
+<?php } else { ?>
+    <p>Erreur Le mot de passe est faux!!!!</p>
+    <a href="formulaire.php">Retour</a>
+<?php } ?>
+
+</html>
